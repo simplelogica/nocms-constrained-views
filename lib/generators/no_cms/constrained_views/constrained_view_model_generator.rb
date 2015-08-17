ee8c00abeb8579d08ce368d74f1ebcd9a4f38567
David J. Brenes <davidjbrenes@gmail.com>
2015-08-17 15:35:21 +0200

    Creating the has_many relationship in our dummy model from the generator

--- lib/generators/no_cms/constrained_views/constrained_view_model_generator.rb
+++ lib/generators/no_cms/constrained_views/constrained_view_model_generator.rb
@@ -5,5 +5,9 @@ class NoCms::ConstrainedViews::ConstrainedViewModelGenerator < Rails::Generators
 
     # And then we inherit from our base model
     gsub_file "app/models/#{singular_name}_constrained_view.rb", /ActiveRecord::Base/, 'NoCms::ConstrainedViews::Base'
+
+    # And create the has_many relationship into the "parent" model
+    gsub_file "app/models/#{singular_name}.rb", /ActiveRecord::Base/, "ActiveRecord::Base\n\n  has_many :constrained_views, class_name: \"#{class_name}ConstrainedView\"\n"
+
   end
 end
