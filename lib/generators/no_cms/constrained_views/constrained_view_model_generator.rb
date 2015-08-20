1ce364b82ab98d200ce16309336353d0b9a77335
David J. Brenes <davidjbrenes@gmail.com>
2015-08-20 10:19:33 +0200

    Setting the relationshipwith blocks in the base model
    This way we avoid opening the file in the Rails generator and our generated model is cleaner

--- lib/generators/no_cms/constrained_views/constrained_view_model_generator.rb
+++ lib/generators/no_cms/constrained_views/constrained_view_model_generator.rb
@@ -11,8 +11,6 @@ class NoCms::ConstrainedViews::ConstrainedViewModelGenerator < Rails::Generators
 
     # And then we inherit from our base model
     gsub_file "app/models/#{class_name.underscore}_constrained_view.rb", /ActiveRecord::Base/, 'NoCms::ConstrainedViews::Base'
-    gsub_file "app/models/#{class_name.underscore}_constrained_view.rb", 'NoCms::ConstrainedViews::Base', "NoCms::ConstrainedViews::Base\n\n" +
-    "  has_and_belongs_to_many :blocks, class_name: \"NoCms::Blocks::Block\", association_foreign_key: 'no_cms_blocks_block_id'\n"
 
     # And create the has_many relationship into the "parent" model
     gsub_file "app/models/#{class_name.underscore}.rb", /ActiveRecord::Base/, "ActiveRecord::Base\n\n  has_many :constrained_views, class_name: \"#{class_name}ConstrainedView\"\n"
--- spec/dummy/app/models/page_constrained_view.rb
+++ spec/dummy/app/models/page_constrained_view.rb
@@ -1,7 +1,5 @@
 class PageConstrainedView < NoCms::ConstrainedViews::Base
 
-  has_and_belongs_to_many :blocks, class_name: "NoCms::Blocks::Block", association_foreign_key: 'no_cms_blocks_block_id'
-
   belongs_to :page
 
   constrain_by :single_date
