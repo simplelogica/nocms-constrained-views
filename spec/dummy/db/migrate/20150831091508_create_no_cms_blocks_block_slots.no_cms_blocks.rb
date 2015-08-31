155594917078d220d89dcfb24cce7aacf2e99be3
David J. Brenes <davidjbrenes@gmail.com>
2015-08-31 11:16:42 +0200

    New migrations from no_cms_blocks

---
+++ spec/dummy/db/migrate/20150831091508_create_no_cms_blocks_block_slots.no_cms_blocks.rb
@@ -0,0 +1,15 @@
+# This migration comes from no_cms_blocks (originally 20150828085904)
+class CreateNoCmsBlocksBlockSlots < ActiveRecord::Migration
+  def change
+    create_table :no_cms_blocks_block_slots do |t|
+      t.belongs_to :container, polymorphic: true, index: {name: 'index_no_cms_blocks_block_slots_on_container_type_and_id'}
+      t.belongs_to :block, index: true
+      t.integer :position, default: 0
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key :no_cms_blocks_blocks, :no_cms_blocks_blocks, column: 'parent_id'
+
+  end
+end
