d36886aa53f240969e59153caa68e654c426e541
David J. Brenes <davidjbrenes@gmail.com>
2015-09-21 12:36:04 +0200

    Changing the single date migration generator

--- lib/generators/no_cms/constrained_views/single_date_migration_generator.rb
+++ lib/generators/no_cms/constrained_views/single_date_migration_generator.rb
@@ -1,6 +1,6 @@
 class NoCms::ConstrainedViews::SingleDateMigrationGenerator < Rails::Generators::NamedBase
   def create_miration
     # First we create the model
-    generate "acts_as_constrained:single_date_migration", "#{singular_name}_constrained_view"
+    generate "acts_as_constrained:single_date_migration #{singular_name}_constrained_view"
   end
 end
