ae9cf5372e170b27d2577573fd6b155b83b924c6
David J. Brenes <davidjbrenes@gmail.com>
2015-12-10 09:17:26 +0100

    Task #1: Updated  nocms_blocks migrations for postgres

--- spec/dummy/db/migrate/20150819150411_create_no_cms_blocks_blocks.no_cms_blocks.rb
+++ spec/dummy/db/migrate/20150819150411_create_no_cms_blocks_blocks.no_cms_blocks.rb
@@ -1,5 +1,4 @@
 # This migration comes from no_cms_blocks (originally 20140405135410)
-# This migration comes from no_cms_blocks (originally 20140405135410)
 class CreateNoCmsBlocksBlocks < ActiveRecord::Migration
   def change
     create_table :no_cms_blocks_blocks do |t|
@@ -8,12 +7,16 @@ class CreateNoCmsBlocksBlocks < ActiveRecord::Migration
     end
 
     NoCms::Blocks::Block.translated_attribute_names = [:locale, :layout, :fields_info, :draft]
-    
+
     create_table :no_cms_blocks_block_translations do |t|
       t.belongs_to :no_cms_blocks_block
       t.string :locale
       t.string :layout
-      t.text :fields_info, :limit => 4294967295
+      if NoCms::Blocks.installed_db_gem == 'pg'
+        t.text :fields_info
+      else
+        t.text :fields_info, :limit => 4294967295
+      end
       t.boolean :draft
     end
 
